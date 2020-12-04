003ef273992d52b5cebc70e8385cba9f797a0a3e
Purmarili <jvash18@freeuni.edu.ge>
2020-12-05 00:21:51 +0400

    generate 4 size

--- src/Range/Main.java
+++ src/Range/Main.java
@@ -7,6 +7,10 @@ import java.util.function.Function;
 public class Main {
     public static void main(String[] args) {
 
+        PasswordIterator it = new PasswordIterator(4);
+        while (it.hasNext())
+            System.out.println(it.next());
+
     }
 
     public static String longToStringWithLength(long value, int length) {
