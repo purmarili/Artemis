19c16e7ab917b9e2a6af6d620281c900b3773ae4
purmarili <jvash18@freeuni.edu.ge>
2021-01-18 22:09:19 +0400

    Threads

--- src/Test/Threads.java
+++ src/Test/Threads.java
@@ -6,12 +6,9 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Threads {
     public static void main(String[] args) {
-//
-//        Lock lock = new ReentrantLock();
-//        Condition cond = lock.newCondition();
 
-        int arr[] = new int[5];
-        int n[] = new int[1];
+        int [] arr = new int[5];
+        int [] n = new int[1];
 
         Writer writer = new Writer(arr, n);
         Writer writer1 = new Writer(arr, n);
---
+++ src/Threads/Table.java
@@ -0,0 +1,23 @@
+package Threads;
+
+public class Table {
+
+	int forks;
+
+	public Table(int forks) {
+		this.forks = forks;
+	}
+
+	public boolean forkAvailable() {
+		return forks > 0;
+	}
+
+	public void takeFork() {
+		forks--;
+	}
+
+	public void returnFork() {
+		forks++;
+	}
+
+}
